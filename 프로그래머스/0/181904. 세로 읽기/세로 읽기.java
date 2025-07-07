6a9643108cd8ed2f70f13d05f7973e7ce5907068
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2025-07-07 16:56:34 +0900

    [level 0] Title: 세로 읽기, Time: 24.10 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -2,17 +2,11 @@ import java.util.*;
 
 class Solution {
     public String solution(String str, int m, int c) {
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < str.length(); i += m) {
-            String sub = str.substring(i, i + m);
-            result.add(sub);
-        }
-        
         String answer = "";
-        for (String str2 : result) {
-            answer += str2.substring(c-1, c);
+
+        for (int i = c - 1; i < str.length(); i += m) {
+            answer += str.charAt(i);
         }
-        
         return answer;
     }
 }
