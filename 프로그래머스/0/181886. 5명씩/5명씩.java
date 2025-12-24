2e684c3c5d87934cbbcaf41ce51dc1e4a534c6cf
전장훈 <104408863+Janghoon33@users.noreply.github.com>
2025-12-24 16:13:06 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public List<String> solution(String[] names) {
+        List<String> list = new ArrayList<>();
+        
+        for(int i = 0; i < names.length; i++){
+            if(i%5 == 0){
+                list.add(names[i]);
+            }
+        }
+        
+        return list;
+    }
+}
